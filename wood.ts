31420d5ec15f4c8b7889e467e50258919c36d0f3
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2024-03-07 10:01:12 -0500

    Create wood.ts

---
+++ wood.ts
@@ -0,0 +1,31 @@
+/*This program calculates the lenght of a board foot of wood
+* By Venika Sem
+* @version 1.0
+* @since Feb-2024
+*/
+
+import { createPrompt } from 'bun-promptx'
+
+function BoardFoot(width = number, height = number) {
+        const volume = 144
+        const length = volume / (width * height)     
+        return length
+}
+
+function main() {
+        const widthString = createPrompt("Enter the width of the piece of wood (in inches): ")
+        const heightString = createPrompt("Enter the height of the piece of wood (in inches): ")
+        
+        const width = parseFloat(widthString.value)
+        const height = parseFloat(heightString.value)  
+
+if (isNaN(width) || isNaN(height) || width <= 0 || height <= 0) {
+        console.log("\nInvalid input. Width and height must be positive numbers.\n")
+        return
+} 
+const length = BoardFoot(width, height)
+console.log(`To make exactly 1 board foot of wood, the length should be ${length.toFixed(1)} inches.`)
+console.log(`\nDone.`)
+}
+// Call main function
+main() 
